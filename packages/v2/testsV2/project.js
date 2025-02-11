607c8f9e08252a6a2c0091ddab4274cdfddfe7f6
z3dev <z3-dev@gfnews.net>
2025-02-11 12:05:58 +0900

    performance(all): sync of project test suites

--- packages/v2/testsV2/project.js
+++ packages/v2/testsV2/project.js
@@ -4,7 +4,6 @@ let sphere_7 = null // 98 points
 let sphere_22 = null // 1012 points
 let sphere_70 = null // 9940 points
 let sphere_158 = null // 50244 points
-let sphere_224 = null // 99904 points
 
 const setupVolume = {
   name: 'measureVolume setup',
@@ -15,7 +14,6 @@ const setupVolume = {
     sphere_22 = primitives.sphere({ radius: 100, segments: 22 })
     sphere_70 = primitives.sphere({ radius: 100, segments: 70 })
     sphere_158 = primitives.sphere({ radius: 100, segments: 158 })
-    sphere_224 = primitives.sphere({ radius: 100, segments: 224 })
     return {}
   }
 }
@@ -41,18 +39,10 @@ const project_sphere_10000 = {
   func: function runme () { return extrusions.project({}, sphere_70) }
 }
 
-const project_sphere_100000 = {
-  name: 'project(100000)',
-  api: 'project(sphere)',
-  div: '100000',
-  func: function runme () { return extrusions.project({}, sphere_224) }
-}
-
 module.exports = {
   setupVolume,
 
   project_sphere_100,
   project_sphere_1000,
   project_sphere_10000
-  // project_sphere_100000
 }
